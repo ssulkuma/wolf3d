c760ba7ee10a9b66db2b21460e3e6f9518719897
Sarita Sulkumaki <ssulkuma@c3r3p3.hive.fi>
2022-07-01 16:03:22 +0300

    bugfix in ft_strjoin.c

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: ssulkuma <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/15 16:26:41 by ssulkuma          #+#    #+#             */
-/*   Updated: 2021/11/15 16:56:07 by ssulkuma         ###   ########.fr       */
+/*   Updated: 2022/07/01 14:32:56 by ssulkuma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,10 @@
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*str;
-	size_t	s1_len;
-	size_t	s2_len;
-	size_t	i;
-	size_t	j;
+	int		s1_len;
+	int		s2_len;
+	int		i;
+	int		j;
 
 	i = 0;
 	j = 0;
@@ -26,7 +26,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		return (NULL);
 	s1_len = ft_strlen(s1);
 	s2_len = ft_strlen(s2);
-	str = (char *)malloc(sizeof(char) * s1_len + s2_len + 1);
+	str = (char *)malloc(sizeof(char) * (s1_len + s2_len + 1));
 	if (!str)
 		return (NULL);
 	while (i < s1_len)
