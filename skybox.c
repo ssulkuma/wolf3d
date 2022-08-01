b90f952c68fe1608bd6f13de8940c67826b0e159
Sarita Sulkumaki <ssulkuma@c3r3p3.hive.fi>
2022-08-01 13:53:54 +0300

    fixed texture scaling

--- skybox.c
+++ skybox.c
@@ -6,7 +6,7 @@
 /*   By: ssulkuma <ssulkuma@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/28 11:32:19 by ssulkuma          #+#    #+#             */
-/*   Updated: 2022/07/29 15:37:44 by ssulkuma         ###   ########.fr       */
+/*   Updated: 2022/08/01 13:52:21 by ssulkuma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,8 @@ void	skybox(t_data *data, t_image *texture)
 	double	result;
 
 	result = atan2(data->player->direction.y, data->player->direction.x);
+	if (result < 0)
+		result = result + 2.0 * PI;
 	result = result * 180.0 / PI;
 	printf("RESULT %f\n", result);
 	step_x = (double)SKYBOX_TEX_WIDTH / 4.0 / (double)WIDTH;
@@ -31,21 +33,23 @@ void	skybox(t_data *data, t_image *texture)
 	printf("STEP X %f\n", step_x);
 	printf("STEP Y %f\n", step_y);
 	tex_y = 0;
-	tex_x = ft_fabs(result);
+	tex_x = 0;
 	y = 0;
 	while (y < HEIGHT)
 	{
 		x = 0;
+		tex_x = 360 - result;
 		while (x < WIDTH)
 		{
 			//printf("TEX X %f\n", tex_x);
-			//printf("TEX Y %f\n\n", tex_y);
+			//printf("X %d\n", x);
+			//printf("Y %d\n\n", y);
 			color = get_pixel_from_image(&texture[5], tex_x, tex_y);
 			draw_pixel_to_image(data->mlx, x, y, color);
 			tex_x += step_x;
-			//tex_y += step_y;
 			x++;
 		}
+		tex_y += step_y;
 		y++;
 	}
 }
